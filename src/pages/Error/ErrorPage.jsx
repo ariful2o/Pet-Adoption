11a1b57c0bb06fcc4501827b977ae60a419c7d07
Mohammad Ariful <arifulz848@gmail.com  git config --global user.name mohammadariful20>
2024-09-15 11:43:13 +0600

    update error page and message

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -6,10 +6,10 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <div id="error-page" className="flex flex-col items-center justify-center min-h-screen space-y-4">
+      <h1 className="text-4xl text-green-600 italic">Oops !</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+      <p className="text-xl font-bold text-red-500"> 
         <i>{error.statusText || error.message}</i>
       </p>
     </div>

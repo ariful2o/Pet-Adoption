bf4a63a1157b884c0adbb5badea03c4f4f5ae30d
Mohammad Ariful <arifulz848@gmail.com  git config --global user.name mohammadariful20>
2024-09-15 13:09:23 +0600

    update carsual image

--- src/pages/home/Home/Carsual.jsx
+++ src/pages/home/Home/Carsual.jsx
@@ -12,10 +12,10 @@ export default function Carsual() {
     >
       <Slider>
         <Slide index={0}>
-          <img className='w-full h-full' src="https://images.contentstack.io/v3/assets/blt6f84e20c72a89efa/blt1473f8fa02cf305c/6261d1c93348304067e0dc0e/img-requirements-to-adopt-a-pet.jpg" alt="" />
+          <img className='w-full h-full' src="https://people.com/thmb/BJO005QLK6f4YcZHW0iONQxoJ1g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/family-adopt-dog-6c2f1eedd593433f85549e94e07af8bf.jpg" alt="" />
         </Slide>
         <Slide index={1}>
-          <img className='w-full h-full' src="https://people.com/thmb/BJO005QLK6f4YcZHW0iONQxoJ1g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/family-adopt-dog-6c2f1eedd593433f85549e94e07af8bf.jpg" alt="" />
+          <img className='w-full h-full' src="https://images.contentstack.io/v3/assets/blt6f84e20c72a89efa/blt1473f8fa02cf305c/6261d1c93348304067e0dc0e/img-requirements-to-adopt-a-pet.jpg" alt="" />
         </Slide>
         <Slide index={2}>
           <img className='w-full h-full' src="https://media-be.chewy.com/wp-content/uploads/2020/10/28102619/pet-adoption-statistics.jpg" alt="" />

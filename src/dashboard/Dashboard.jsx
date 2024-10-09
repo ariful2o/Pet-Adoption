112b96eb81e8a5a529321d0b8936d1c62c743ece
Mohammad Ariful <arifulz848@gmail.com  git config --global user.name mohammadariful20>
2024-10-09 14:01:20 +0600

    responsive dashboard list

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -17,90 +17,123 @@ import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../hooks/isAdmin/useAdmin";
 
 export default function Dashboard() {
-    const [isAdmin,isPending]=useAdmin();
+    const [isAdmin, isPending] = useAdmin();
+
+    const navlists = <>
+
+
+        <div className="">
+            {isAdmin &&
+
+                <>
+                    <li>
+                        <NavLink to="/dashboard/adminhome">
+                            <FaHome />
+                            Admin Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/allpets">
+                            <FaList />
+                            All Pets
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/alldonations">
+                            <FaSackDollar />
+                            All Donations
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/users">
+                            <FaUsers />
+                            All Users
+                        </NavLink>
+                    </li>
+                </>
+            }
+
+            <>
+                <li>
+                    <NavLink to="/">
+                        <FcHome />
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard/addpet">
+                        <MdPets />
+                        Add a pet
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard/mypet">
+                        <MdPlaylistAddCheckCircle />
+                        My added pets
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard/adoprionrequest">
+                        <IoIosGitPullRequest />
+                        Adoption Request
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard/donationform">
+                        <MdOutlineCampaign />
+                        Create Donation Campaign
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard/mycampaigns">
+                        <SiBookmyshow />
+                        My Donation Campaigns
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard/mydonations">
+                        <LiaDonateSolid />
+                        My Donations
+                    </NavLink>
+                </li>
+            </>
+        </div>
+    </>
 
     return (
         <div className="flex">
             <div className="">
-                {isAdmin &&
-                    <div className="bg-[#28282f] max-w-60 p-1  text-white">
-                        <ul className="space-y-4 menu uppercase">
-                            <>
-                                <li>
-                                    <NavLink to="/dashboard">
-                                        <FaHome />
-                                        Admin Home
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/allpets">
-                                        <FaList />
-                                        All Pets
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/alldonations">
-                                      <FaSackDollar />
-                                        All Donations 
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/users">
-                                        <FaUsers />
-                                        All Users
-                                    </NavLink>
-                                </li>
-                            </>
+
+                <div className="navbar bg-base-100">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h8m-8 6h16" />
+                                </svg>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                {navlists}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 lg:min-h-screen">
+                            {navlists}
                         </ul>
                     </div>
-                }
-                <div className={isAdmin ? "bg-[#1d1d3e] max-w-60 p-5 text-white" : "bg-[#1d1d3e] max-w-60 p-5 text-white h-screen items-center flex flex-row"}>
-                    <ul className="space-y-4 menu uppercase">
-                        <>
-                            <li>
-                                <NavLink to="/">
-                                   <FcHome />
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/addpet">
-                                   <MdPets />
-                                    Add a pet
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/mypet">
-                                    <MdPlaylistAddCheckCircle />
-                                    My added pets
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/adoprionrequest">
-                                    <IoIosGitPullRequest />
-                                    Adoption Request
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/donationform">
-                                   <MdOutlineCampaign  />
-                                  Create Donation Campaign
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/mycampaigns">
-                                   <SiBookmyshow />
-                                     My Donation Campaigns
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/mydonations">
-                                   <LiaDonateSolid />
-                                    My Donations
-                                </NavLink>
-                            </li>
-                        </>
-                    </ul>
+
                 </div>
             </div>
             <div className="flex-1 bg-[#E8E8E8]">
@@ -109,3 +142,7 @@ export default function Dashboard() {
         </div>
     )
 }
+
+
+
+

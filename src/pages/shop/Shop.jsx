5944036702c245efa21da88b42ed2101c728edd9
Mohammad Ariful <arifulz848@gmail.com  git config --global user.name mohammadariful20>
2024-10-11 14:47:10 +0600

    add contacts page and google map

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Shop() {
   return (
-    <div><img className='w-full h-1/2 md:h-screen' src="https://d3jmn01ri1fzgl.cloudfront.net/photoadking/webp_thumbnail/light-gray-and-black-line-vector-paper-coming-soon-logo-design-template-r7nolc9bb3f82a.webp" alt="" /></div>
+    <div><img className='w-full h-1/2 md:h-screen' src="https://t4.ftcdn.net/jpg/08/07/96/25/360_F_807962566_RalqY14KS9wzNL0M5bONj6rthlpJ4e2k.jpg" alt="" /></div>
   )
 }
